9007f223f528d651ce89688a0dcb7aa29f2489fb
james peres <james.peres.s@gmail.com>
2017-04-06 23:21:33 -0300

    pep8

--- script.py
+++ script.py
@@ -5,6 +5,6 @@ def count(num):
         number = int(num)
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
-        
+
     case_for_mouth = (number / 30)
     return case_for_mouth
